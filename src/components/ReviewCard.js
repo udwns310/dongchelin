b1f5709cd898ea40380312c4b2546f36cc3ebba6
udwns310 <uroasecret5@gmail.com>
2024-06-19 21:33:36 +0900

    리뷰 등록 api 연동

--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -1,41 +1,38 @@
-import React from "react";
-import { Card, Col, Row } from "antd";
-import Rating from "@mui/material/Rating";
+import React from 'react';
+import { Card, Col, Row } from 'antd';
+import Rating from '@mui/material/Rating';
 
 const cardStyle = {
-  width: "100%",
-  textAlign: "left",
-  height: "20vh",
+    width: '100%',
+    textAlign: 'left',
+    height: '20vh',
 };
 
 function ReviewCard(props) {
-  const reviews = props.reviews;
-
-  if (reviews) {
-    return (
-      <div>
-        <Row gutter={16} style={cardStyle}>
-          {Array.from({
-            length: 6,
-          }).map((_, i) => (
-            <Col span={8}>
-              <Card
-                title={reviews[0].user.nickname}
-                extra={
-                  <Rating name="read-only" value={reviews[0].rate} readOnly />
-                }
-                bordered={true}
-                style={{ border: "1px solid grey" }}
-              >
-                {reviews[0].comment}
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      </div>
-    );
-  } else {
-    return null;
-  }
+    const reviews = props.reviews;
+    if (reviews) {
+        return (
+            <div>
+                <Row gutter={16} style={cardStyle}>
+                    {Array.from({
+                        length: reviews.length > 6 ? 6 : reviews.length,
+                    }).map((_, i) => (
+                        <Col span={8}>
+                            <Card
+                                title={reviews[i].user.nickname}
+                                extra={<Rating name="read-only" value={reviews[i].rate} readOnly />}
+                                bordered={true}
+                                style={{ border: '1px solid grey' }}
+                            >
+                                {reviews[i].comment}
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+            </div>
+        );
+    } else {
+        return null;
+    }
 }
 export default ReviewCard;
--- src/review/ReviewHeader.js
+++ src/review/ReviewHeader.js
@@ -1,98 +1,78 @@
-import React from "react";
-import Image from "react-bootstrap/Image";
-import Badge from "react-bootstrap/Badge";
-import { Carousel } from "antd";
-import { useState } from "react";
-import Rating from "@mui/material/Rating";
+import React from 'react';
+import Image from 'react-bootstrap/Image';
+import Badge from 'react-bootstrap/Badge';
+import { Carousel } from 'antd';
+import { useState } from 'react';
+import Rating from '@mui/material/Rating';
 
 const contentStyle = {
-  height: "300px",
-  color: "#fff",
-  lineHeight: "160px",
-  textAlign: "center",
-  background: "#364d79",
-  width: "33.3%",
-  display: "inline",
+    height: '300px',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#364d79',
+    width: '33.3%',
+    display: 'inline',
 };
 
 function ReviewHeader(props) {
-  const [badgeStyle, setbadgeStyle] = useState("");
+    const [badgeStyle, setbadgeStyle] = useState('');
 
-  const food_id = props.food_id;
-  const food_img = props.food_img;
-  const avgRate = props.avgRate;
-  const title = props.title;
-  const restaurant = props.restaurant;
-  return (
-    <div>
-      <Carousel autoplay>
+    const food_id = props.food_id;
+    const food_img = props.food_img;
+    const avgRate = props.avgRate;
+    const title = props.title;
+    const restaurant = props.restaurant;
+    return (
         <div>
-          <Image
-            src={process.env.PUBLIC_URL + "/img/" + food_img + ".jpg"}
-            style={contentStyle}
-          />
-          <Image
-            src={process.env.PUBLIC_URL + "/img/" + food_img + ".jpg"}
-            style={contentStyle}
-          />
-          <Image
-            src={process.env.PUBLIC_URL + "/img/" + food_img + ".jpg"}
-            style={contentStyle}
-          />
-        </div>
-        <div>
-          <Image
-            src={process.env.PUBLIC_URL + "/img/" + food_img + ".jpg"}
-            style={contentStyle}
-          />
-          <Image
-            src={process.env.PUBLIC_URL + "/img/" + food_img + ".jpg"}
-            style={contentStyle}
-          />
-          <Image
-            src={process.env.PUBLIC_URL + "/img/" + food_img + ".jpg"}
-            style={contentStyle}
-          />
-        </div>
-      </Carousel>
-      <div>
-        <Image src="img/best3.png" style={{ width: "50px", height: "50px" }} />
-        <h1 style={{ display: "inline", fontSize: "50px", padding: "15px" }}>
-          {title}
-        </h1>
-        <p
-          style={{
-            display: "block",
-            fontSize: "50px",
-            color: "orange",
-          }}
-        >
-          <Rating name="read-only" value={avgRate} readOnly />
-        </p>
+            <Carousel autoplay>
+                <div>
+                    <Image src={process.env.PUBLIC_URL + '/img/' + food_img + '.jpg'} style={contentStyle} />
+                    <Image src={process.env.PUBLIC_URL + '/img/' + food_img + '.jpg'} style={contentStyle} />
+                    <Image src={process.env.PUBLIC_URL + '/img/' + food_img + '.jpg'} style={contentStyle} />
+                </div>
+                <div>
+                    <Image src={process.env.PUBLIC_URL + '/img/' + food_img + '.jpg'} style={contentStyle} />
+                    <Image src={process.env.PUBLIC_URL + '/img/' + food_img + '.jpg'} style={contentStyle} />
+                    <Image src={process.env.PUBLIC_URL + '/img/' + food_img + '.jpg'} style={contentStyle} />
+                </div>
+            </Carousel>
+            <div>
+                <Image src="img/best3.png" style={{ width: '50px', height: '50px' }} />
+                <h1 style={{ display: 'inline', fontSize: '50px', padding: '15px' }}>{title}</h1>
+                <p
+                    style={{
+                        display: 'block',
+                        fontSize: '50px',
+                        color: 'orange',
+                    }}
+                >
+                    <Rating name="read-only" value={avgRate} precision={0.1} readOnly />
+                </p>
 
-        <Badge bg={badgeStyle} style={{ fontSize: "20px" }}>
-          <BadgeName />
-        </Badge>
-      </div>
-    </div>
-  );
+                <Badge bg={badgeStyle} style={{ fontSize: '20px' }}>
+                    <BadgeName />
+                </Badge>
+            </div>
+        </div>
+    );
 
-  function BadgeName() {
-    var name = restaurant;
-    switch (name) {
-      case "suduk":
-        setbadgeStyle("success");
-        return "수덕전";
-      case "tech":
-        setbadgeStyle("warning");
-        return "정보관";
-      case "dormitory":
-        setbadgeStyle("danger");
-        return "기숙사식당";
-      default:
-        return null;
+    function BadgeName() {
+        var name = restaurant;
+        switch (name) {
+            case 'suduk':
+                setbadgeStyle('success');
+                return '수덕전';
+            case 'tech':
+                setbadgeStyle('warning');
+                return '정보관';
+            case 'dormitory':
+                setbadgeStyle('danger');
+                return '기숙사식당';
+            default:
+                return null;
+        }
     }
-  }
 }
 
 export default ReviewHeader;
